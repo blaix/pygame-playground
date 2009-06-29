e935356236e8c46019cf73fa65f0c1de7b2155bf
Justin Blake <jblake@zampano.local>
2009-06-28 23:53:14 -0400

    fullscreen mode!

--- movement.py
+++ movement.py
@@ -1,18 +1,19 @@
 character_image_filename = 'images/bob.png'
-speed = 2
-screen_width = 640
-screen_height = 480
+SPEED = 1
+SCREEN_SIZE = (800, 600)
 
 import pygame
 from pygame.locals import *
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
-character = pygame.image.load(character_image_filename).convert()
+screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
+character = pygame.image.load(character_image_filename).convert_alpha()
+font = pygame.font.SysFont("Verdana", 16)
 
 x, y = 0, 0
 move_x, move_y = 0, 0
+fullscreen = 0
 
 while True:
     
@@ -20,14 +21,20 @@ while True:
         if event.type == QUIT:
             exit()
         if event.type == KEYDOWN:
+            if event.key == K_f:
+                if fullscreen:
+                    fullscreen = 0
+                else:
+                    fullscreen = FULLSCREEN
+                screen = pygame.display.set_mode(SCREEN_SIZE, fullscreen, 32)
             if event.key == K_LEFT:
-                move_x = -speed
+                move_x = -SPEED
             elif event.key == K_RIGHT:
-                move_x = +speed
+                move_x = +SPEED
             elif event.key == K_UP:
-                move_y = -speed
+                move_y = -SPEED
             elif event.key == K_DOWN:
-                move_y = +speed
+                move_y = +SPEED
         elif event.type == KEYUP:
             if event.key == K_LEFT or event.key == K_RIGHT:
                 move_x = 0
@@ -38,6 +45,7 @@ while True:
     y += move_y
     
     screen.fill((0, 0, 0))
+    screen.blit(font.render("Hit 'f' to enter/exit fullscreen mode.", True, (250, 250, 250)), (5, 5))
     screen.blit(character, (x, y))
     
     pygame.display.update()
